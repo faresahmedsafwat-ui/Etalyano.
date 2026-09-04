583eb9248f8a41693ef70945fee9a7e24401b391
faresahmedsafwat-ui <faresahmedsafwat@gmail.com>
2026-09-04 08:42:45 +0300

    Delete scripte.js

--- scripte.js
+++
@@ -1,8 +0,0 @@
-// زرار "قراءة" اللي بيظهر المقال
-document.querySelectorAll('.toggle-btn').forEach(button => {
-  button.addEventListener('click', () => {
-    const para = button.nextElementSibling;
-    para.classList.toggle('hidden');
-    button.textContent = para.classList.contains('hidden') ? 'قراءة' : 'إخفاء';
-  });
-});
